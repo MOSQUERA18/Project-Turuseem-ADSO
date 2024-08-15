8b58cfd022b93ef27697bc235b6060cb146037b0
Mosquera <sarmientomarlon452@gmail.com>
2024-08-15 12:09:36 -0500

    Despues de Hashear el Id_User no salen registros

--- back-end/src/helpers/generarJWT.js
+++ back-end/src/helpers/generarJWT.js
@@ -2,35 +2,10 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 // Clave secreta para el JWT
-const JWT_SECRET = process.env.JWT_SECRET;
 
-export const generarJWT = async (Id_User) => {
-  // Generar un hash del Id_User
-  const hashedId_User = await bcrypt.hash(Id_User.toString(), 10);
-
-  // Payload del JWT con el hash
-  const tokenPayload = {
-    hashedId_User
-  };
-
-  // Firmar el token con el secreto JWT
-  return jwt.sign(tokenPayload, JWT_SECRET, { expiresIn: '1h' });
+export const generarJWT = (Id_User) => {
+  return jwt.sign({ Id_User:  Id_User}, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 };
-
-
-// export const verificarJWT = async (token) => {
-//   try {
-//     // Verificar el token JWT
-//     const decoded = jwt.verify(token, JWT_SECRET);
-    
-//     // Aquí no desencriptamos sino que verificamos el hash
-//     const hashedId_User = decoded.hashedId_User;
-
-//     // Aquí puedes retornar el hash o realizar alguna acción adicional
-//     return hashedId_User;
-//   } catch (error) {
-//     throw new Error('Token inválido');
-//   }
-// };
-
 
