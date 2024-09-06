60ddc2da1e99380e66ac21dcdc6de0d6829af792
Marlon Mosquera <sarmientomarlon452@gmail.com>
2024-09-06 14:54:54 -0500

    Cambie color a botones desde el config

--- front-end/src/programasFormacion/CrudProgramaFormacion.jsx
+++ front-end/src/programasFormacion/CrudProgramaFormacion.jsx
@@ -184,7 +184,7 @@ const resetForm = () => {
 
   return (
     <>
-    <h1 className="text-black font-extrabold text-4xl md:text-4xl text-center mb-7"> Gestionar Informacion de los Programas de Formacion</h1>
+    <h1 className="text-black font-extrabold text-4xl md:text-4xl text-center mb-7"> Gestionar Informacion de los <span className="text-blue-700"> Programas de Formacion</span></h1>
       
 
         {/* <button
