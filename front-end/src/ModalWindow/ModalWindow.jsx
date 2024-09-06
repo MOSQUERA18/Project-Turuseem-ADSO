cff634c3909329402678f119789f6ea1e1b65554
Marlon Mosquera <sarmientomarlon452@gmail.com>
2024-09-06 14:34:58 -0500

    Cambie color a El boton de Agregar de manera Forzada

--- front-end/src/ModalWindow/ModalWindow.jsx
+++ front-end/src/ModalWindow/ModalWindow.jsx
@@ -33,13 +33,16 @@ function ModalWindow({ stateAddNewRow, setStateAddNewRow, form, isModalOpen, set
     <>
       {/* Botón para abrir el modal */}
       <button
-        onClick={() => [setStateAddNewRow(!stateAddNewRow), toggleModal()]}
-        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-      >
-        <IoMdPersonAdd />
-        Agregar
-      </button>
+  onClick={() => [setStateAddNewRow(!stateAddNewRow), toggleModal()]}
+  className="flex items-center gap-2 bg-blue-700 text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold rounded-lg text-sm px-5 py-2.5 shadow-lg transition-colors duration-300 ease-in-out"
+  type="button"
+  style={{ color: 'white' }}  // Esto asegura que el color del texto sea blanco
+>
+  <IoMdPersonAdd size={20} />
+  Agregar
+</button>
+
+
 
       {/* Modal */}
       {isOpen && (
