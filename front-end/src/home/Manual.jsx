624d1a92f25e6c485247ec5387e36c3d99111da2
Marlon Mosquera <sarmientomarlon452@gmail.com>
2024-10-07 16:23:35 -0500

    Imagenes de la public actualizadas

--- front-end/src/home/Manual.jsx
+++ front-end/src/home/Manual.jsx
@@ -7,7 +7,7 @@ function Manual() {
           <span className="text-botones">Turnos</span>
         </h1>
         <iframe
-          src="Public/PDFs/Manual-Consulta-TURUSEEM.pdf"
+          src="/PDFs/Manual-Consulta-TURUSEEM.pdf"
           height="600"  // Ajuste de altura
           className="w-3/4"  // Ajuste de ancho al 75% del contenedor
 
